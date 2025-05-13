9845170853c52383f1022967fe73e2941d6829af
LilSeabass08 <sashi.ethington@gmail.com>
2025-05-13 14:39:16 -0600

    first commit

---
+++ android/app/src/main/java/baby/food/MainActivity.java
@@ -0,0 +1,5 @@
+package baby.food;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'baby.food',
+  appName: 'baby-food',
+  webDir: 'dist'
+};
+
+export default config;
